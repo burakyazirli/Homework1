8211b427284999296c4f3a1739cdebe3dd9badb9
burak <burak@DESKTOP-97LKUT5>
2024-10-14 22:54:27 +0300

    Add project files.

---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable 
+
+namespace BLL.DAL
+{
+    public class Students
+    {
+        //private int id;
+
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+        //public int GetId() 
+        //{
+        //    return this.id;
+        //}
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public decimal Gpa { get; set; }
+
+    }
+}
